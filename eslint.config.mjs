8c2d6a1948995772ecfc95a9bcb37faaa881c041
douglasmtss <douglasmatosdev@gmail.com>
2026-01-04 16:15:04 -0300

    fix: update ESLint configuration to include TypeScript support and add @typescript-eslint/eslint-plugin

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,9 +2,11 @@ import { defineConfig, globalIgnores } from 'eslint/config'
 import nextVitals from 'eslint-config-next/core-web-vitals'
 import prettier from 'eslint-config-prettier/flat'
 import prettierConfig from 'eslint-plugin-prettier/recommended'
+import tseslint from 'typescript-eslint'
 
 const eslintConfig = defineConfig([
     ...nextVitals,
+    ...tseslint.configs.recommended,
     prettier,
     prettierConfig,
     // Override default ignores of eslint-config-next.
@@ -13,15 +15,15 @@ const eslintConfig = defineConfig([
             'object-curly-spacing': ['warn', 'always'],
             'no-return-assign': 'off',
             'next-line space-infix-ops': 'off',
-            'eqeqeq': ['warn', 'smart'],
-            'curly': ['warn', 'multi-line', 'consistent'],
+            eqeqeq: ['warn', 'smart'],
+            curly: ['warn', 'multi-line', 'consistent'],
             'react/react-in-jsx-scope': 'off',
             'react-hooks/rules-of-hooks': 'error',
             'react-hooks/exhaustive-deps': 'warn',
             'newline-before-return': 'warn',
             '@typescript-eslint/no-use-before-define': 'off',
             '@typescript-eslint/explicit-module-boundary-types': 'off',
-            '@typescript-eslint/explicit-function-return-type': 'error',
+            '@typescript-eslint/explicit-function-return-type': 'error'
         }
     },
     globalIgnores([
